21d88196c78e12026b3975accf143c6f29974f5d
ArtefactX1 <123149085+ArtefactX1@users.noreply.github.com>
2025-12-11 13:49:39 +0700

    Delete client directory

--- client/src/pages/AdminProfilePage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-function AdminProfilePage() {
-  return <h1>Halaman Profil Saya (Placeholder)</h1>;
-}
-export default AdminProfilePage;
--- client/src/pages/AdminSettingsPage.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-function AdminSettingsPage() {
-  return <h1>Halaman Pengaturan (Placeholder)</h1>;
-}
-export default AdminSettingsPage;
